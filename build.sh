8a8742c8729928c4c82ce815cb123d64e69411a7
DespairFactor <matthewalex@outlook.com>
2020-04-24 21:49:05 -0400

    build: V4P2

--- build.sh
+++ build.sh
@@ -10,16 +10,16 @@ clear
 
 # Resources
 THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
-export CLANG_PATH=~/android/clang/clang-r365631c/bin
+export CLANG_PATH=~/android/clang/clang-r377782d/bin
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=~/android/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=${HOME}/android/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-export LD_LIBRARY_PATH=${HOME}/android/clang/clang-r365631c/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${HOME}/android/clang/clang-r377782d/lib64:$LD_LIBRARY_PATH
 DEFCONFIG="wahoo_defconfig"
 
 # Kernel Details
-VER=".V3P2"
+VER=".V4P2"
 
 # Paths
 KERNEL_DIR=`pwd`
