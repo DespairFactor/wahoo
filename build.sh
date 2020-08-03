945ef0a3fe05c37018f411b3a3842e59f8491b6b
DespairFactor <matthewalex@outlook.com>
2020-08-03 14:07:27 -0400

    build: V3P2

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ export LD_LIBRARY_PATH=${HOME}/android/clang/clang-r383902c/lib64:$LD_LIBRARY_PA
 DEFCONFIG="wahoo_defconfig"
 
 # Kernel Details
-VER=".V2P2"
+VER=".V3P2"
 
 # Paths
 KERNEL_DIR=`pwd`
